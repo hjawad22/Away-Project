00f86c10c225bcc5e4c1e7092d6279f31ffeefa1
Houda Jawad <houdajawad@gmail.com>
2023-04-21 08:36:08 -0700

    Add fetch calls to api file

---
+++ src/apiCalls.js
@@ -0,0 +1,16 @@
+function fetchTravelers() {
+    return fetch("http://localhost:3001/api/v1/travelers")
+      .then(response => response.json());
+  };
+  
+  function fetchTrips() {
+    return fetch("http://localhost:3001/api/v1/trips")
+      .then((response) => response.json());
+  };
+  
+  function fetchDestinations() {
+    return fetch("http://localhost:3001/api/v1/destinations")
+      .then((response) => response.json());
+  };
+  
+  export {fetchTravelers, fetchTrips, fetchDestinations}
